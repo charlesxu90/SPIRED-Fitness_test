b0e6a0921c2308cb7283060719ee81707798e3e1
Xiaopeng Xu <charlesxu90@gmail.com>
2024-04-07 10:28:09 +0300

    update test script

--- run_stab_pred.sh
+++ run_stab_pred.sh
@@ -1,4 +1,4 @@
 # conda activate spired_fitness
-# python run_SPIRED-Stab.py --fasta_file data/example/test.fasta --saved_folder results/example
+python run_SPIRED-Stab.py --fasta_file data/example/test.fasta --saved_folder results/example
 
-python run_SPIRED-Stab.py --fasta_file data/example/test2.fasta --saved_folder results/example2
+# python run_SPIRED-Stab.py --fasta_file data/example/test2.fasta --saved_folder results/example2
