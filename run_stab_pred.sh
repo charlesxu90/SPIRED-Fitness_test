fa49bbf1366a947470971c3d0bd428d9f92612e9
xux <xux@kaust.edu.sa>
2025-02-04 21:22:01 +0300

    save changes

--- run_stab_pred.sh
+++ run_stab_pred.sh
@@ -1,6 +1,23 @@
 # conda activate spired_fitness
-python run_SPIRED-Stab.py --fasta_file data/example/test.fasta --saved_folder results/example
+# python run_SPIRED-Stab.py --fasta_file data/example/test.fasta --saved_folder results/example
 
 
 # python run_SPIRED-Stab.py --fasta_file results/savi_bsj_r2/mut_combs_1k_SPF.fasta --saved_folder results/savi_bsj_r2/
 # python run_SPIRED-Stab.py --fasta_file results/savi_bsj_r2/top1k_cluster_merged_SPF.fasta --saved_folder results/savi_bsj_r2/
+# python run_SPIRED-Stab.py --fasta_file results/savi_bsj_r2/mcs_uniq_join_duplicated-yh-top_merged_top100.fasta --saved_folder results/savi_bsj_r2/
+
+# python run_SPIRED-Stab.py --fasta_file results/savi_bsj_r3/data_plm_filt_SPF.fasta --saved_folder results/savi_bsj_r3/
+
+# python run_SPIRED-Stab.py --fasta_file results/savi_bsj_r3/m20_I80_extend_data_plm_filt_SPF.fasta --saved_folder results/savi_bsj_r3/
+# python run_SPIRED-Stab.py --fasta_file results/savi_bsj_r3/m20_I70_extend_data_plm_filt_SPF.fasta --saved_folder results/savi_bsj_r3/
+
+# python run_SPIRED-Stab.py --fasta_file results/savi_bsj_r3/m20_I70_data_plm_filt_SPF.fasta --saved_folder results/savi_bsj_r3/
+# python run_SPIRED-Stab.py --fasta_file results/savi_bsj_r3/m20_I80_data_plm_filt_SPF.fasta --saved_folder results/savi_bsj_r3/
+# python run_SPIRED-Stab.py --fasta_file results/savi_bsj_r3/m20_I90_data_plm_filt_SPF.fasta --saved_folder results/savi_bsj_r3/
+
+# python run_SPIRED-Stab.py --fasta_file results/savi_bsj_r4/data.scored.zy.m317_1.topn.1k.spired.fasta --saved_folder results/savi_bsj_r4/
+# python run_SPIRED-Stab.py --fasta_file results/savi_bsj_r4/data.scored.zy.open.topn.1k.spired.fasta --saved_folder results/savi_bsj_r4/
+
+python run_SPIRED-Stab.py --fasta_file results/savi_r4_test_spiredstab/R4_BSJ_wetlab.act_stab.csv_uniq.csv.fasta --saved_folder results/savi_r4_test_spiredstab/
+# python run_SPIRED-Stab.py --fasta_file results/savi_r4_test_spiredstab/R4_ML1_APP.xlsx.app.csv_uniq.csv.fasta --saved_folder results/savi_r4_test_spiredstab/
+# python run_SPIRED-Stab.py --fasta_file results/savi_r4_test_spiredstab/R4_XP_wetlab.xlsx.act_stab_uniq.csv.fasta --saved_folder results/savi_r4_test_spiredstab/
